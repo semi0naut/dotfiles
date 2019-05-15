d4c36b7a4e3e1484ac23650384bbf185664106d4
Michael Campagnaro <mikecampo@gmail.com>
2019-05-15 13:51:15 -0400

    Remove some old stuff I no longer use

--- scripts/ruby/rails/tmux-rails-template.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-tmux new -d -s my-awesome-app
-tmux new-window -t my-awesome-app:2 -n 'server' 'bundle exec rails server'
-tmux new-window -t my-awesome-app:3 -n 'vim' 'vi'
-tmux attach -t my-awesome-app
